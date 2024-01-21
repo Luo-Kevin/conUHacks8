93d3a490b6bb9b6b110929fcedf7a723995f97e5
qinxuanx <112572108+qinxuanx@users.noreply.github.com>
2024-01-20 23:41:30 -0500

    report cards

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
-import DropFile from '../components/DropFile';
-import Calendar from '../components/Calendar';
-import useFile from '../hooks/useFile';
-import { postFile } from '../utils/api';
-import { Heading, Button } from '@chakra-ui/react'
+import React from "react";
+import DropFile from "../components/DropFile";
+import Calendar from "../components/Calendar";
+import useFile from "../hooks/useFile";
+import { postFile } from "../utils/api";
+import { Heading, Button } from "@chakra-ui/react";
 
-import './App.css';
+import "./App.css";
+import ReportCards from "../components/ReportCards";
 
 function App() {
   const { readFile, data, title } = useFile();
 
   const handleSubmit = () => {
     postFile(data);
-  }
+  };
 
   return (
     <div className="main-container">
-      <div className='landing-section'>
-        <Heading as='h1' noOfLines={1}>
+      <div className="landing-section">
+        <Heading as="h1" noOfLines={1}>
           Repair Service Scheduler
         </Heading>
       </div>
-      <div className='file-upload-section'>
-        <div className='file-upload-section-div'>
+      <div className="file-upload-section">
+        <div className="file-upload-section-div">
           <DropFile className="file-upload-section" readFile={readFile} title={title} />
         </div>
       </div>
-      <Button className='upload-file-button' onClick={handleSubmit} mx="auto" display="block">Upload</Button>
-      <Calendar/>
+      <Button className="upload-file-button" onClick={handleSubmit} mx="auto" display="block">
+        Upload
+      </Button>
+
+      <ReportCards />
+
+      <Calendar />
     </div>
   );
 }
