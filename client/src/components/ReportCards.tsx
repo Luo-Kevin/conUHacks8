910d2597ab6e3c2a42f876505a65de689d626d3f
qinxuanx <112572108+qinxuanx@users.noreply.github.com>
2024-01-21 02:56:03 -0500

    connect backend to report cards

--- client/src/components/ReportCards.tsx
+++ client/src/components/ReportCards.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import { Heading, Card, CardBody, Text, Divider } from "@chakra-ui/react";
 import { SimpleGrid } from "@chakra-ui/layout";
 
-const ReportCards = () => {
+type ReportCardsProps = {
+  revenue: number;
+  lost_revenue: number;
+  count_turnover: number;
+  compact_turnover: number;
+  medium_turnover: number;
+  full_size_turnover: number;
+  class_1_turnover: number;
+  class_2_turnover: number;
+  count_serviced: number;
+  compact_serviced: number;
+  medium_serviced: number;
+  full_size_serviced: number;
+  class_1_serviced: number;
+  class_2_serviced: number;
+}
+
+const ReportCards = (reportCardsProps: ReportCardsProps) => {
   return (
     <div className="report-cards-grid">
       <SimpleGrid columns={2} spacing={5}>
@@ -13,16 +30,16 @@ const ReportCards = () => {
             </Heading>
             <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
             <Text fontSize={"lg"} as={"b"}>
-              Number of Vehicles Served:{" "}
+              Number of Vehicles Served: {reportCardsProps.count_serviced}
             </Text>
-            <Text>Compact Cars: </Text>
-            <Text>Medium Cars: </Text>
-            <Text>Full-Size Cars: </Text>
-            <Text>Class 1 Trucks: </Text>
-            <Text>Class 2 Trucks: </Text>
+            <Text>Compact Cars: {reportCardsProps.compact_serviced} </Text>
+            <Text>Medium Cars: {reportCardsProps.medium_serviced}</Text>
+            <Text>Full-Size Cars: {reportCardsProps.full_size_serviced} </Text>
+            <Text>Class 1 Trucks: {reportCardsProps.class_1_serviced}</Text>
+            <Text>Class 2 Trucks: {reportCardsProps.class_2_serviced}</Text>
             <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
             <Text fontSize={"lg"} as={"b"}>
-              Total Revenue:{" "}
+              Total Revenue: {reportCardsProps.revenue} $
             </Text>
           </CardBody>
         </Card>
@@ -33,16 +50,16 @@ const ReportCards = () => {
             </Heading>
             <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
             <Text fontSize={"lg"} as={"b"}>
-              Number of Vehicles Declined:{" "}
+              Number of Vehicles Declined: {reportCardsProps.count_turnover}
             </Text>
-            <Text>Compact Cars: </Text>
-            <Text>Medium Cars: </Text>
-            <Text>Full-Size Cars: </Text>
-            <Text>Class 1 Trucks: </Text>
-            <Text>Class 2 Trucks: </Text>
+            <Text>Compact Cars: {reportCardsProps.compact_turnover} </Text>
+            <Text>Medium Cars: {reportCardsProps.medium_turnover}</Text>
+            <Text>Full-Size Cars: {reportCardsProps.full_size_turnover} </Text>
+            <Text>Class 1 Trucks: {reportCardsProps.class_1_turnover}</Text>
+            <Text>Class 2 Trucks: {reportCardsProps.class_2_turnover}</Text>
             <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
             <Text fontSize={"lg"} as={"b"}>
-              Revenue Lost:{" "}
+              Revenue Lost: {reportCardsProps.lost_revenue} $
             </Text>
           </CardBody>
         </Card>
--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -17,20 +17,27 @@ function App() {
     LOADED,
   }
   const [loadingStatus, setLoadingStatus] = React.useState<LoadingStatus>(LoadingStatus.INIT);
+  const [results, setResults] = React.useState<any>(null);
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
+    if (!data) {
+      return;
+    }
+
     setLoadingStatus(LoadingStatus.LOADING);
-    setTimeout(() => {
+    const response = await postFile(data);
+    if (response) {
+      setResults(response);
       setLoadingStatus(LoadingStatus.LOADED);
-    }, 1000);
-    postFile(data);
+      console.log(response);
+    }
   };
 
   return (
     <div className="main-container">
       <div className="landing-section">
         <Heading as="h1" noOfLines={1}>
-          Repair Service Scheduler
+          Tire Repair Service Scheduler
         </Heading>
       </div>
       <div className="file-upload-section">
@@ -38,9 +45,16 @@ function App() {
           <DropFile className="file-upload-section" readFile={readFile} title={title} />
         </div>
       </div>
-      <Button className="upload-file-button" onClick={handleSubmit} mx="auto" display="block">
+      <Button
+        isDisabled={loadingStatus === LoadingStatus.LOADING}
+        className="upload-file-button"
+        onClick={handleSubmit}
+        mx="auto"
+        display="block"
+      >
         Upload
       </Button>
+      
       {loadingStatus === LoadingStatus.LOADING && (
         <div className="spinner">
           <Spinner thickness="4px" emptyColor="gray.200" color="blue.500" size="xl" />
@@ -49,7 +63,22 @@ function App() {
       {loadingStatus === LoadingStatus.LOADED && (
         <div className="result-section">
           <div className="result-section-cards">
-            <ReportCards />
+            <ReportCards
+              revenue={results.revenue}
+              lost_revenue={results.lost_revenue}
+              count_turnover={results.count_turnover}
+              compact_turnover={results.compact_turnover}
+              medium_turnover={results.medium_turnover}
+              full_size_turnover={results.full_size_turnover}
+              class_1_turnover={results.class_1_turnover}
+              class_2_turnover={results.class_2_turnover}
+              count_serviced={results.count_serviced}
+              compact_serviced={results.compact_serviced}
+              medium_serviced={results.medium_serviced}
+              full_size_serviced={results.full_size_serviced}
+              class_1_serviced={results.class_1_serviced}
+              class_2_serviced={results.class_2_serviced}
+            />
           </div>
           <div className="result-section-calendar">
             <Calendar />
