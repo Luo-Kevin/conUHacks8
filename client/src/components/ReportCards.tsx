2bfb8668d31a12d84096f376613caa6299525751
qinxuanx <112572108+qinxuanx@users.noreply.github.com>
2024-01-21 00:47:32 -0500

    spinner

--- client/src/components/ReportCards.tsx
+++ client/src/components/ReportCards.tsx
@@ -4,48 +4,50 @@ import { SimpleGrid } from "@chakra-ui/layout";
 
 const ReportCards = () => {
   return (
-    <SimpleGrid columns={2} spacing={5}>
-      <Card>
-        <CardBody>
-          <Heading size="lg" color={"green"}>
-            Served Vehicles Report
-          </Heading>
-          <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
-          <Text fontSize={"lg"} as={"b"}>
-            Number of Vehicles Served:{" "}
-          </Text>
-          <Text>Compact Cars: </Text>
-          <Text>Medium Cars: </Text>
-          <Text>Full-Size Cars: </Text>
-          <Text>Class 1 Trucks: </Text>
-          <Text>Class 2 Trucks: </Text>
-          <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
-          <Text fontSize={"lg"} as={"b"}>
-            Total Revenue:{" "}
-          </Text>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <Heading size="lg" color={"red"}>
-            Declined Vehicles Report
-          </Heading>
-          <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
-          <Text fontSize={"lg"} as={"b"}>
-            Number of Vehicles Declined:{" "}
-          </Text>
-          <Text>Compact Cars: </Text>
-          <Text>Medium Cars: </Text>
-          <Text>Full-Size Cars: </Text>
-          <Text>Class 1 Trucks: </Text>
-          <Text>Class 2 Trucks: </Text>
-          <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
-          <Text fontSize={"lg"} as={"b"}>
-            Revenue Lost:{" "}
-          </Text>
-        </CardBody>
-      </Card>
-    </SimpleGrid>
+    <div className="report-cards-grid">
+      <SimpleGrid columns={2} spacing={5}>
+        <Card>
+          <CardBody>
+            <Heading size="lg" color={"green"}>
+              Served Vehicles Report
+            </Heading>
+            <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
+            <Text fontSize={"lg"} as={"b"}>
+              Number of Vehicles Served:{" "}
+            </Text>
+            <Text>Compact Cars: </Text>
+            <Text>Medium Cars: </Text>
+            <Text>Full-Size Cars: </Text>
+            <Text>Class 1 Trucks: </Text>
+            <Text>Class 2 Trucks: </Text>
+            <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
+            <Text fontSize={"lg"} as={"b"}>
+              Total Revenue:{" "}
+            </Text>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <Heading size="lg" color={"red"}>
+              Declined Vehicles Report
+            </Heading>
+            <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
+            <Text fontSize={"lg"} as={"b"}>
+              Number of Vehicles Declined:{" "}
+            </Text>
+            <Text>Compact Cars: </Text>
+            <Text>Medium Cars: </Text>
+            <Text>Full-Size Cars: </Text>
+            <Text>Class 1 Trucks: </Text>
+            <Text>Class 2 Trucks: </Text>
+            <Divider marginTop={"0.5em"} marginBottom={"0.5em"} />
+            <Text fontSize={"lg"} as={"b"}>
+              Revenue Lost:{" "}
+            </Text>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
+    </div>
   );
 };
 
--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -3,7 +3,7 @@ import DropFile from "../components/DropFile";
 import Calendar from "../components/Calendar";
 import useFile from "../hooks/useFile";
 import { postFile } from "../utils/api";
-import { Heading, Button } from "@chakra-ui/react";
+import { Heading, Button, Spinner } from "@chakra-ui/react";
 
 import "./App.css";
 import ReportCards from "../components/ReportCards";
@@ -11,8 +11,19 @@ import ReportCards from "../components/ReportCards";
 function App() {
   const { readFile, data, title } = useFile();
 
+  enum LoadingStatus {
+    INIT,
+    LOADING,
+    LOADED,
+  }
+  const [loadingStatus, setLoadingStatus] = React.useState<LoadingStatus>(LoadingStatus.INIT);
+
   const handleSubmit = () => {
-    postFile(data);
+    setLoadingStatus(LoadingStatus.LOADING);
+    setTimeout(() => {
+      setLoadingStatus(LoadingStatus.LOADED);
+    }, 1000);
+    //postFile(data);
   };
 
   return (
@@ -30,10 +41,21 @@ function App() {
       <Button className="upload-file-button" onClick={handleSubmit} mx="auto" display="block">
         Upload
       </Button>
-
-      <ReportCards />
-
-      <Calendar />
+      {loadingStatus === LoadingStatus.LOADING && (
+        <div className="spinner">
+          <Spinner thickness="4px" emptyColor="gray.200" color="blue.500" size="xl" />
+        </div>
+      )}
+      {loadingStatus === LoadingStatus.LOADED && (
+        <div className="result-section">
+          <div className="result-section-cards">
+            <ReportCards />
+          </div>
+          <div className="result-section-calendar">
+            <Calendar />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
